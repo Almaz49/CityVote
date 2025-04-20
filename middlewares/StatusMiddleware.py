9de91210d7276b44fd7f715e24876239b73f0488
Aleksey Mazur <avmazur@gmail.com>
2025-04-20 21:30:57 +0700

    меняю logging на logger

--- middlewares/StatusMiddleware.py
+++ middlewares/StatusMiddleware.py
@@ -8,40 +8,40 @@ logger = logging.getLogger(__name__)
 
 class StatusMiddleware(BaseMiddleware):
     async def __call__(self, handler, event, data):
-        logging.info('\nMiddleware StatusMiddleware начала работу\n')
+        logger.info('\nMiddleware StatusMiddleware начала работу\n')
         try:
             user = data['event_from_user']
             user_id = user.id
 
-            logging.debug(f'Processing event of type: {type(event)}')
-            logging.info(f'User ID: {user_id}')
+            logger.debug(f'Processing event of type: {type(event)}')
+            logger.info(f'User ID: {user_id}')
 
             # Проверяем, доступен ли пользователь
             is_available = await is_user_available(user_id)
-            logging.debug(f"Пользователь {user_id} доступен: {is_available}")
+            logger.debug(f"Пользователь {user_id} доступен: {is_available}")
 
             # Если пользователь был недоступен, но теперь взаимодействует с ботом, помечаем его как доступного
             if not is_available:
-                logging.info(f"Пользователь {user_id} ранее был недоступен, но теперь взаимодействует с ботом. Меняем статус на доступен.")
+                logger.info(f"Пользователь {user_id} ранее был недоступен, но теперь взаимодействует с ботом. Меняем статус на доступен.")
                 await mark_user_as_available(user_id)
 
             # Получаем статус пользователя
             status = await status_member(user_id)
-            logging.debug(f'Получены статусы юзера: {status}')
+            logger.debug(f'Получены статусы юзера: {status}')
 
             if status is None:
-                logging.warning(f"Status for user {user_id} is None")
+                logger.warning(f"Status for user {user_id} is None")
                 status = []  # Если статус отсутствует, используем пустой список
             else:
                 status = [s.lower() for s in status]  # Нормализуем статусы к нижнему регистру
 
             # Добавляем статус в пользовательский словарь data
             data['data']['user_status'] = status
-            logging.info(f'Создан список статусов в пользовательском словаре data: {status}')
+            logger.info(f'Создан список статусов в пользовательском словаре data: {status}')
 
             # Продолжаем обработку события
             return await handler(event, data)
         except Exception as e:
-            logging.error(f"An error occurred in StatusMiddleware: {e}")
-            logging.error(f"Event type: {type(event)}, Event data: {event}")
+            logger.error(f"An error occurred in StatusMiddleware: {e}")
+            logger.error(f"Event type: {type(event)}, Event data: {event}")
             return await handler(event, data)
