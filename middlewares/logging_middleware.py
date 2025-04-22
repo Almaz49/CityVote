4f29db213f3df75c6f02e3719934302c38179daf
Aleksey Mazur <avmazur@gmail.com>
2025-04-22 21:34:06 +0700

    меняю logging на logger

--- middlewares/logging_middleware.py
+++ middlewares/logging_middleware.py
@@ -11,21 +11,21 @@ logger = logging.getLogger(__name__)
 
 class LoggingAndErrorHandlingMiddleware(BaseMiddleware):
     async def __call__(self, handler, event: Update, data: dict):
-        # logging.info(f"Data in LoggingAndErrorHandlingMiddleware: {pformat(data)}\n")
-        logging.info('\n Middleware logging_middleware started work\n')
+        # logger.info(f"Data in LoggingAndErrorHandlingMiddleware: {pformat(data)}\n")
+        logger.info('\n Middleware logging_middleware started work\n')
         try:
             if isinstance(event, Update):
                 user = data['event_from_user']
                 tg_id = user.id
                 # tg_id = event.from_user.id if hasattr(event, "from_user") and event.from_user else "Unknown"
                 event_type = event.__class__.__name__
-                logging.info(f"Получено событие {event_type} от пользователя {tg_id}") #:\n {pformat(event)}\n")
+                logger.info(f"Получено событие {event_type} от пользователя {tg_id}") #:\n {pformat(event)}\n")
 
             club_id = data.get('club_id',None)
             user_id, member_id = await extract_user_member_id(tg_id)
 
             if 'state' in data:
-                 logging.info(f"Middleware detected FSM state before handler: {await data['state'].get_state()}\n")
+                 logger.info(f"Middleware detected FSM state before handler: {await data['state'].get_state()}\n")
 
             # Создаем ключ 'data', если его еще нет
             if 'data' not in data:
@@ -36,23 +36,23 @@ class LoggingAndErrorHandlingMiddleware(BaseMiddleware):
 
                     # Добавьте другие необходимые данные
                 }
-                logging.info(f'Создан словарь дата в мидлваре логирования {pformat(data["data"])}')
+                logger.info(f'Создан словарь дата в мидлваре логирования {pformat(data["data"])}')
 
             # Логируем callback_data или текст сообщения
             if hasattr(event, "message") and event.message:
-                logging.info(f"\n\nПолучено текстовое сообщение: {event.message.text}")
+                logger.info(f"\n\nПолучено текстовое сообщение: {event.message.text}")
             elif hasattr(event, "callback_query") and event.callback_query:
-                logging.info(f"\n\nПолучен callback_query с данными: {event.callback_query.data}")
+                logger.info(f"\n\nПолучен callback_query с данными: {event.callback_query.data}")
             else:
-                logging.info(f"\n\nПолучено событие другого типа: {type(event)}")
+                logger.info(f"\n\nПолучено событие другого типа: {type(event)}")
 
 
             response = await handler(event, data)
 
-            # logging.info(f'Данные data после прохождения хэндлера: \n{pformat(data)}\n')
+            # logger.info(f'Данные data после прохождения хэндлера: \n{pformat(data)}\n')
             if 'state' in data:
-                 logging.info(f"\nMiddleware detected FSM state after handler: {await data['state'].get_state()}")
-                 logging.info(f"Middleware detected FSM data after handler: {await data['state'].get_data()}\n")
+                 logger.info(f"\nMiddleware detected FSM state after handler: {await data['state'].get_state()}")
+                 logger.info(f"Middleware detected FSM data after handler: {await data['state'].get_data()}\n")
 
 
 
@@ -60,7 +60,7 @@ class LoggingAndErrorHandlingMiddleware(BaseMiddleware):
 
 
         except Exception as e:
-            logging.error(f"Необработанное исключение в хэндлере: {e}\n{traceback.format_exc()}")
+            logger.error(f"Необработанное исключение в хэндлере: {e}\n{traceback.format_exc()}")
             try:
                 user = data['event_from_user']
                 tg_id = user.id
