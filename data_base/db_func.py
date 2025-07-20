aa9f400b8b384bfba86a2358def56ccf4cb9a286
Aleksey Mazur <avmazur@gmail.com>
2025-07-20 09:09:00 +0700

    пофиксил ошибку в SQL запросе

--- data_base/db_func.py
+++ data_base/db_func.py
@@ -302,7 +302,7 @@ async def get_profile(member_id: int):
                     m.description,
                     m.info_level,
                     t.token AS token,
-                    proxy_user.username AS proxy_username,
+                    proxy_user.username AS proxy_username
                 FROM Members m
                 INNER JOIN Users u ON m.user_id = u.id
                 LEFT JOIN Tokens t ON m.token = t.id
